65ba8f0650bf0aed5f2a267a4e9aa8fda140ade4
Benjamin Lee <benlee.poh.huat@gmail.com>
2020-04-21 14:31:30 +0800

    include npm and tsc files

---
+++ src.ts/index.ts
@@ -0,0 +1,5 @@
+import { FiniteStateMachine } from './finite-state-machine';
+import { FsmState } from './fsm-state';
+import { FsmEvent } from './fsm-event';
+
+export { FiniteStateMachine, FsmState, FsmEvent };
